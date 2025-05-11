88a7461625046a6db35a96c612e103f08c50869d
Benjamin Mickler <benmickler4@gmail.com>
2025-05-11 16:07:02 +1000

    Update build-release script

--- build-release.sh
+++ build-release.sh
@@ -8,7 +8,7 @@ do
     for arch in "${archs[@]}"
     do
         echo "Building $platform - $arch"
-        env GOOS=$platform GOARCH=$arch go build -o release/sendip-client-$platform-$arch -ldflags "-s -w" client/main.go
-        env GOOS=$platform GOARCH=$arch go build -o release/sendip-server-$platform-$arch -ldflags "-s -w" server/main.go
+        env GOOS=$platform GOARCH=$arch go build -o release/sendip-client-$platform-$arch -ldflags "-s" client/main.go
+        env GOOS=$platform GOARCH=$arch go build -o release/sendip-server-$platform-$arch -ldflags "-s" server/main.go
     done
 done
